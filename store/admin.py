5299e89a6009429a1c5bdf83fe89fd45f5a143fa
Paras sirohi <parassirohi000@gmail.com>
2023-01-22 21:29:36 +0530

    Managing image in the admin

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,3 @@
-
 # from django.contrib.contenttypes.admin import GenericTabularInline
 # from tags.models import TaggedItem
 from django.contrib import admin, messages
@@ -24,6 +23,7 @@ class InventoryFilter(admin.SimpleListFilter):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
 
+
 @admin.register(models.Collection)
 @admin.display(ordering='product_count')
 class CollectionAdmin(admin.ModelAdmin):
@@ -32,44 +32,60 @@ class CollectionAdmin(admin.ModelAdmin):
 
     def product_count(self, collection):
         # return collection.product_count # we don't have field with this name in collection
-    # for that we need to override queryset on this page and annotate product_count
+        # for that we need to override queryset on this page and annotate product_count
 
         url = (reverse('admin:store_product_changelist')
-            + '?'
-            + urlencode({
+               + '?'
+               + urlencode({
                     'collection__id': str(collection.id)
-                })) # this is where we need to generate the query string parameters to apply filter,
+                }))  # this is where we need to generate the query string parameters to apply filter,
         # for that we import
-        #another utility function urlencode
-        return format_html('<a href="{}"> {} </a>', url, collection.product_count) # to provide
+        # another utility function urlencode
+        return format_html('<a href="{}"> {} </a>', url, collection.product_count)  # to provide
         # link to product we use format_html
-
 
     # there is no filter on product list to apply a filter we need to add (?collection__id=1) into url
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            product_count = Count('products')
+            product_count=Count('products')
         )
 
 
-#customisation the list
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage  # register this with admin class
+    readonly_fields = ['thumbnail']
+
+    # thumbnail is not one of the fields of the product image claSS, so we need to define method
+
+    def thumbnail(self, instance):  # now we take that product image and convert it to an HTML page
+        if instance.image.name != '':  # we use condition to check whether we have image or not
+            return format_html(f'<img src="{instance.image.url}" class="custom-thumbnail" />')
+        # we use format_html here coz we only get url not an image in our admin thumbnail,
+        # now this image is way too big this is where we use css, this is a css class that we define in static folder
+        return ''
+
+
+# customisation the list
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     # inlines = [TagInline]
+    inlines = [ProductImageInline]
+
     search_fields = ['title']
     autocomplete_fields = ['collection']
     prepopulated_fields = {
-        'slug': ['title'] # we are passing a list, so we can add multiple fields
+        'slug': ['title']  # we are passing a list, so we can add multiple fields
     }
 
     actions = ['clear_inventory']
     list_filter = ['collection', 'last_update', InventoryFilter]
 
-    list_display = ['title', 'unit_price', 'inventory_status','collection_title'] # used to display given object, we can
+    list_display = ['title', 'unit_price', 'inventory_status',
+                    'collection_title']  # used to display given object, we can
     # also add related object to our display, like we add collection and if we want to show special field
     # for that we need to define method by that name
-    list_editable = ['unit_price']# used to edit given list object
+    list_editable = ['unit_price']  # used to edit given list object
     list_per_page = 10
     list_select_related = ['collection']
 
@@ -83,9 +99,9 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'Ok'
 
-    @admin.action(description= 'Clear inventory')
+    @admin.action(description='Clear inventory')
     def clear_inventory(self, request, queryset):  # we are defining this function to add
-                                                    # more option in admin's action
+        # more option in admin's action
         updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
@@ -93,11 +109,16 @@ class ProductAdmin(admin.ModelAdmin):
             messages.ERROR
         )
 
+    class Media:  # with this class we can specify the static assets that should be loaded on the product admin page
+        # , we can load css or jawa script file. this class just like Meta class.
+        css = {
+            'all': ['store/styles.css']
+        } # this is how we can load our style sheet or our css file on the product admin page
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
     list_select_related = ['user']
@@ -107,11 +128,11 @@ class CustomerAdmin(admin.ModelAdmin):
     @admin.display(ordering='orders_count')
     def orders(self, customer):
         url = (
-            reverse('admin:store_order_changelist')
-            + '?'
-            + urlencode({
-                'customer__id': str(customer.id)
-            }))
+                reverse('admin:store_order_changelist')
+                + '?'
+                + urlencode({
+            'customer__id': str(customer.id)
+        }))
         return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
 
     def get_queryset(self, request):
@@ -127,13 +148,12 @@ class OrderItemInline(admin.TabularInline):
     autocomplete_fields = ['product']
     extra = 0
 
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
     autocomplete_fields = ['customer']
     list_display = ['id', 'placed_at', 'customer']
 
-
 # admin.site.register(models.Collection)
 # admin.site.register(models.Product, ProductAdmin) # we can one way and second way on above
-
